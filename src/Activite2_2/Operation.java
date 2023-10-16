7f4f25f708e039bed1af6855cbf92efc09283d5f
MariamGargouri <148018137+MariamGargouri@users.noreply.github.com>
2023-10-16 23:54:24 +0100

    Update Operation.java

--- src/Activite2_2/Operation.java
+++ src/Activite2_2/Operation.java
@@ -1,5 +1,40 @@
 package Activite2_2;
+//Activite 2-2
+import java.io.Serializable;
 
-public class Operation {
+public class Operation implements Serializable {
+	private int op1,op2,result;
+	private char op ; //'+','-','+'*','/'
+	
+	public Operation (int op1, int op2,char op)
+	{
+		this.op1=op1;
+		this.op2=op2;
+		this.op=op;
+	}
+	public int getOp1()
+	{
+		return (op1);
+	}
+	public int getOp2()
+	{
+		return (op2);
+	}
+	public char getOp()
+	{
+		return (op);
+	}
+	public int getResult()
+	{
+		return (result);
+	}
+	public void setResult (int result)
+	{
+		 this.result=result;
+	}
+	
+	
+	
+	
 
 }
